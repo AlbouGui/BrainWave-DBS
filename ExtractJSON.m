aab8fda1eb77e2d800cc80ca299120432163c7f5
AlbouGui <albert.guillemette@umontreal.ca>
2025-06-06 17:51:15 -0400

    Add files via upload

---
+++ ExtractJSON.m
@@ -0,0 +1,17 @@
+% Author: Albert Guillemette BSc MSc, 02.06.2025
+% ExtractJSON extracts BrainSense Streaming data from a JSON file
+
+% Select a JSON file to extract
+[filename, data_pathname] = uigetfile('*.json', 'Select a .json file');
+
+if isequal(filename, 0)
+    error('No file selected. Extraction aborted.');
+end
+
+% Load JSON data
+fullpath = fullfile(data_pathname, filename);
+data = jsondecode(fileread(fullpath));
+
+% Save the output to a .mat file
+[~, name, ~] = fileparts(filename);  % Get the file name without extension
+save(fullfile(data_pathname, [name '_output.mat']), 'data');
